3909294b941e5244e544f1a8ebbc62e6c85d3f3f
Ane794 <Ane794@outlook.com>
2022-11-21 08:42:38 +0800

    feat: 修改主題名稱獲取方法

--- chrome-fitting-gnome.sh
+++ chrome-fitting-gnome.sh
@@ -3,23 +3,8 @@
 # Google Chrome 程序名.
 GOOGLE_CHROME=google-chrome
 
-# 設置了 GNOME 主題的文件
-CONFIG_FILE=$HOME/.config/gtk-3.0/settings.ini
-
-# 獲取主題名稱 Settings.gtk-theme-name.
-while read LINE; do
-  if [[ $LINE =~ \[.*\] ]]; then
-    SECTION=$(echo $LINE | sed "s/\[\(.*\)\]/\1/g")
-  fi
-  if [[ $SECTION != Settings ]]; then
-    continue
-  fi
-
-  if [[ $LINE =~ gtk-theme-name=.* ]]; then
-    GTK_THEME_NAME=$(echo $LINE | sed "s/gtk-theme-name=\(.*\)/\1/g")
-    break
-  fi
-done <$CONFIG_FILE
+# 獲取顏色主題名稱.
+GTK_COLOR_SCHEME=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 # 根據主題名稱啓動 Google Chrome.
 if [[ $GTK_COLOR_SCHEME =~ .*dark.* ]]; then # 若主題名稱包含 `dark`
