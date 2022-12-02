bd012c78fc6c87da808be7f731d28ddb0886603c
Ane794 <46439819+Ane794@users.noreply.github.com>
2022-12-03 03:35:34 +0800

    feat: `設置` 等 Chrome 頁面自動應用深色主體

--- chrome-fitting-gnome.sh
+++ chrome-fitting-gnome.sh
@@ -8,7 +8,7 @@ GTK_COLOR_SCHEME=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 # 根據主題名稱啓動 Google Chrome.
 if [[ $GTK_COLOR_SCHEME =~ .*dark.* ]]; then # 若主題名稱包含 `dark`
-  "$GOOGLE_CHROME" --force-dark-mode "$@"
+  "$GOOGLE_CHROME" --force-dark-mode --enable-features=WebUIDarkMode "$@"
 else
   "$GOOGLE_CHROME" "$@"
 fi
